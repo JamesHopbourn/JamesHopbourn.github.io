9d58502e6a66763eb363ba449799e1709af1967e
JamesHopbourn <hopbourncontact@gmail.com>
2022-06-03 17:23:54 +0800

    calculatorData.sh sed replace
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

--- source/220619/running/calculatorData.sh
+++ source/220619/running/calculatorData.sh
@@ -6,6 +6,6 @@ sed '2s/--// ; 17s/--// ; 50s/--// ; 55s/--//' 个人比赛计时.sql|mysql --lo
 
 mv $HOME/docker/mysql/data/running/*.json ../data/
 
-cat ../data/man.json|jq length|xargs echo '136 =='
-cat ../data/woman.json|jq length|xargs echo '68 =='
-cat ../data/overall.json|jq length|xargs echo '204 =='
+cat ../data/man.json|jq length|sed 's|136|✅ man|'
+cat ../data/woman.json|jq length|sed 's|68|✅ man|'
+cat ../data/overall.json|jq length|sed 's|204|✅ overall|'
