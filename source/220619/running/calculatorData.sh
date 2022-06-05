6471cc35040567067325e0b6034659b4dbb6cb00
JamesHopbourn <hopbourncontact@gmail.com>
2022-06-05 09:58:29 +0800

    calculatorData.sh
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

--- source/220619/running/calculatorData.sh
+++ source/220619/running/calculatorData.sh
@@ -1,8 +1,8 @@
 # mysql_config_editor set --login-path=pro --host=127.0.0.1 --port=4406 -uroot -p
 
-sed "/json_arrayagg/ s/--// ; /tag:man/ s/--//" 个人比赛计时.sql|mysql --login-path=pro running
-sed "/json_arrayagg/ s/--// ; /tag:woman/ s/--//" 个人比赛计时.sql|mysql --login-path=pro running
-sed "/json_arrayagg/ s/--// ; /tag:overall/ s/--//" 个人比赛计时.sql|mysql --login-path=pro running
+sed '/json_arrayagg/ s/--// ; /tag:man/ s/--//' 个人比赛计时.sql|mysql --login-path=pro running
+sed '/json_arrayagg/ s/--// ; /tag:woman/ s/--//' 个人比赛计时.sql|mysql --login-path=pro running
+sed '/json_arrayagg/ s/--// ; /tag:overall/ s/--//' 个人比赛计时.sql|mysql --login-path=pro running
 
 mv $HOME/docker/mysql/data/running/*.json ../data/
 
